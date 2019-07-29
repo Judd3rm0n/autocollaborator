2ce6a65a12f37f4fda710a2bd85d3e08ccb7089c
judd3r0 <53295819+judd3r0@users.noreply.github.com>
2019-07-29 18:17:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -540,7 +540,7 @@ fi
 
 
 EOF
-chmod +x /usr/bin/autocollaborator
+chmod +x /usr/local/bin/autocollaborator
 
 echo "Removing any prerouting on target ports........" && sleep 2
 iptables -t nat -D PREROUTING -i ens3 -p udp --dport 53 -j REDIRECT --to-port 3353
